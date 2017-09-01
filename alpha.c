f39d605b85af256a67196901b7ce80003f12634b
milkydarling <31534904+milkydarling@users.noreply.github.com>
2017-09-01 14:46:41 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char m;
+printf("enter m value");
+scanf("%c",&m);
+if('q'||'w'||'r'||'t'||'y'||'u'||'i'||'o'||'p'||'a'||'s'||'d'||'f'||'g'||'h'||'j'||'k'||'l'||'z'||'x')
+{
+printf("alphabet");
+}
+}
