d47d1154240089ba59ca052e96816af590d00b45
milkydarling <31534904+milkydarling@users.noreply.github.com>
2017-09-08 16:51:20 +0530

    Create iso.c

---
+++ iso.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+string string_1;
+string string_2;
+gets(string_1);
+gets(string_2);
+if (areIsomorphic(string_1, string_2))
+{
+printf("isomorphic");
+}
+else
+{
+printf("error");
+}
+
